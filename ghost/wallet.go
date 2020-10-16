080dbdd2d6db8709077037d2a92c5eac0c917967
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2020-10-16 03:41:18 -0400

    Fix StubTpool and StubTpool

--- ghost/wallet.go
+++ ghost/wallet.go
@@ -2,19 +2,22 @@ package ghost
 
 import (
 	"gitlab.com/NebulousLabs/Sia/crypto"
-	"gitlab.com/NebulousLabs/Sia/modules"
 	"gitlab.com/NebulousLabs/Sia/types"
 )
 
 type StubWallet struct{}
 
-func (StubWallet) NewWalletAddress() (uh types.UnlockHash, err error)                       { return }
-func (StubWallet) SignTransaction(*types.Transaction, []crypto.Hash) (err error)            { return }
-func (StubWallet) UnspentOutputs(bool) (us []modules.UnspentOutput, err error)              { return }
-func (StubWallet) UnconfirmedParents(types.Transaction) (ps []types.Transaction, err error) { return }
-func (StubWallet) UnlockConditions(types.UnlockHash) (uc types.UnlockConditions, err error) { return }
+func (StubWallet) Address() (_ types.UnlockHash, _ error) { return }
+func (StubWallet) FundTransaction(*types.Transaction, types.Currency) (_ []crypto.Hash, _ error) {
+	return
+}
+func (StubWallet) SignTransaction(txn *types.Transaction, toSign []crypto.Hash) error {
+	txn.TransactionSignatures = append(txn.TransactionSignatures, make([]types.TransactionSignature, len(toSign))...)
+	return nil
+}
 
 type StubTpool struct{}
 
-func (StubTpool) AcceptTransactionSet([]types.Transaction) (err error) { return }
-func (StubTpool) FeeEstimate() (min, max types.Currency, err error)    { return }
+func (StubTpool) AcceptTransactionSet([]types.Transaction) (_ error)                    { return }
+func (StubTpool) UnconfirmedParents(types.Transaction) (_ []types.Transaction, _ error) { return }
+func (StubTpool) FeeEstimate() (_, _ types.Currency, _ error)                           { return }
